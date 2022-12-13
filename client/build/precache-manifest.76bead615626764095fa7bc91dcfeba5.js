57920fe70f817b0372f7be332ce89fe47062b387
aasthakumar96 <42064198+aasthakumar96@users.noreply.github.com>
2022-12-13 13:04:52 +0530

    Add files via upload

---
+++ client/build/precache-manifest.76bead615626764095fa7bc91dcfeba5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364ac011b866e30091523eb15648c1f5",
+    "url": "/ui/index.html"
+  },
+  {
+    "revision": "2da7c1b8c57455f1a0f5",
+    "url": "/ui/static/css/2.c876b8a2.chunk.css"
+  },
+  {
+    "revision": "b9bc582b5377e1f1932e",
+    "url": "/ui/static/css/main.6bfc6a81.chunk.css"
+  },
+  {
+    "revision": "2da7c1b8c57455f1a0f5",
+    "url": "/ui/static/js/2.078ddf3e.chunk.js"
+  },
+  {
+    "revision": "0a84e9e94506e1203f17990263ed4d64",
+    "url": "/ui/static/js/2.078ddf3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9bc582b5377e1f1932e",
+    "url": "/ui/static/js/main.0320e6dc.chunk.js"
+  },
+  {
+    "revision": "d0499b7654ed78e44f4a",
+    "url": "/ui/static/js/runtime-main.ac96cff3.js"
+  },
+  {
+    "revision": "a205cfb05ac073113f8bc70877b42bb3",
+    "url": "/ui/static/media/PQuA 2021 Templates.a205cfb0.zip"
+  },
+  {
+    "revision": "99faade219e90128d54294dfec55a2a7",
+    "url": "/ui/static/media/logo.99faade2.png"
+  }
+]);
